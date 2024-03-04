41a8645a9d9c4b805c7ef52ad5aaf7bcdd067667
rizanisfu <rizansf21@gmail.com>
2024-03-04 17:33:47 +0700

    updated task

--- problem2.2/main.py
+++ problem2.2/main.py
@@ -1,1 +1,14 @@
-bilangan = int(input())
+#bilangan = int(input())
+
+print('Ini Adalah Program Pencari Faktor Bilangan')
+print('==========================================')
+
+bilangan = int(input("Faktor dari bilangan: "))
+hasil = []
+for i in range(1,bilangan+1):
+    if bilangan % i == 0:
+        hasil.append(i)
+    hasil.sort(reverse=True)
+
+for faktor in hasil:
+    print(faktor)
