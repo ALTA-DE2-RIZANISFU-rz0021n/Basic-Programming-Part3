b8ba57a11d639985f59904e85849fe48cc3068a3
rizanisfu <rizansf21@gmail.com>
2024-03-04 14:27:36 +0700

    updated task

--- problem1/main.py
+++ problem1/main.py
@@ -3,5 +3,24 @@ student_score = 80
 
 # Process: Your Solution Code Here
 
-
 # Output "Nilai A"
+
+print('Berikut Adalah Program Konversi Nilai')
+print('-------------------------------------')
+
+nama_siswa = input('nama siswa : ')
+nilai = input('nilai : ')
+nilai = eval(nilai)
+
+if nilai >= 80:
+    na = 'A'
+elif nilai >= 65:
+    na = 'B+'
+elif nilai >= 50:
+    na = 'B'
+elif nilai >= 35:
+    na = 'C'
+else:
+    na = 'D'
+
+print(f'Konversi nilai siswa : {na}')
