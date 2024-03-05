ef75ece7317d692946e140456de01fad1deb57f4
rizanisfu <rizansf21@gmail.com>
2024-03-05 21:51:12 +0700

    updated task

--- problem3/main.py
+++ problem3/main.py
@@ -1,9 +1,21 @@
-def prime_number(num):
-    return "error response'
 
-if __name__ == '__main__':
-    print(prime_number(11)) # "Prime"
-    print(prime_number(13)) # "Prime"
-    print(prime_number(17)) # "Prime"
-    print(prime_number(20)) # "Not Prime"
-    print(prime_number(35)) # "Not Prime"
+def prime_number(num):
+    if num <= 1:
+        return False
+    elif num <= 3:
+        return True
+    elif num % 2 == 0 or num % 3 == 0:
+        return False
+    
+    i = 5
+    while i * i <= num:
+        if num % i == 0 or num % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+print(prime_number(11)) # "Prime"
+print(prime_number(13)) # "Prime"
+print(prime_number(17)) # "Prime"
+print(prime_number(20)) # "Not Prime"
+print(prime_number(35)) # "Not Prime"
